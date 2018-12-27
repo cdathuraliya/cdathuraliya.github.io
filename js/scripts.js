90051d3ef128dc5b7ae4ab45e0ab936702dcc476
CD Athuraliya <cdathuraliya@gmail.com>
2018-12-28 04:09:04 +0530

    Update news template and content

--- js/scripts.js
+++ js/scripts.js
@@ -129,4 +129,30 @@ $(document).ready(function () {
     //$('.navbar').toggleClass('bg-dark bg-light', $(this).scrollTop() < 50);
     $('.navbar').toggleClass('navbar-dark navbar-light', $(this).scrollTop() < 50);
   });
+
+  // Multi-item carousel scripts
+  // https://www.codeply.com/go/s3I9ivCBYH
+  $('#news-carousel').on('slide.bs.carousel', function (e) {
+    /*
+    CC 2.0 License Iatek LLC 2018
+    Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $('#news-carousel .carousel-item').length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // Append slides to the end
+        if (e.direction == "left") {
+          $('#news-carousel .carousel-item').eq(i).appendTo('#news-carousel .carousel-inner');
+        } else {
+          $('#news-carousel .carousel-item').eq(0).appendTo('#news-carousel .carousel-inner');
+        }
+      }
+    }
+  });
+
 });
